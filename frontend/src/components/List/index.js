38e2a04df55ed9cb93719b322cbba25b078d2c34
marsha.prawira <marsha.prawira@pinhome.id>
2021-11-24 18:27:52 +0700

    feat: done all without latihan

---
+++ frontend/src/components/List/index.js
@@ -0,0 +1,21 @@
+import react from "react";
+import Item from "../Item/index.js";
+
+export default function list({ title, items, onItemClick, isShopList }) {
+    return (
+        <>
+            <h3 style={StyleSheet.heading}>{title}</h3>
+            <div className="list-group">
+                {items.length === 0 ? (
+                    <div className="text-center">
+                        <h1>Belum ada item yang dipilih</h1>
+                        <h3>Klik salah satu item di List item</h3>
+                    </div>
+                ) : null}
+                {items.map((item) => (
+                    <Item key={item.id} item={item} onChange={onItemClick} isShopList={isShopList} />
+                ))}
+            </div>
+        </>
+    );
+}
